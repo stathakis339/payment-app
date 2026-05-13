28798352bd2e53caa9e13e8ccbd8150fa7edee58
El. Stathakis <stathakislefteris@outlook.com>
2026-05-13 15:51:56 +0300

    Add GitHub Pages deploy

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+
+base: '/payment-app/',
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/payment-app/',
 });
